feed9b4b141125e33396af50921211fa0c866aa7
Abbas Khan <abbaskhan@Abbass-MacBook-Air.local>
2025-03-25 17:26:11 -0400

    feat: setup storybook + design system grids

---
+++ src/stories/components/usecase-hero/Index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles  from './style.module.scss';
+
+export interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  primary?: boolean;
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: 'small' | 'medium' | 'large';
+  /** Button contents */
+  label: string;
+  /** Optional click handler */
+  onClick?: () => void;
+}
+
+/** Primary UI component for user interaction */
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+  return (
+    <button
+      type="button"
+      className={styles.hero}
+      {...props}
+    >
+      {label}
+      <style jsx>{`
+        button {
+          background-color: ${backgroundColor};
+        }
+      `}</style>
+    </button>
+  );
+};
