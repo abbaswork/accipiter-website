a3b126251c8236df5327789e46400fc274123a75
abdullahdoeswork <abdullahasadwork@gmail.com>
2025-05-04 01:15:07 -0400

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import {Navbar} from "../stories/components/navbar/Index";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      
+
       <main className={styles.main}>
         <Image
           className={styles.logo}
