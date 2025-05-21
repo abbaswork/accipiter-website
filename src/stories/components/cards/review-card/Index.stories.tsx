d6e26eb5e63f7d872876d31eea80cfb3b8db8947
Abbas Khan <abbaskhan@Abbass-MacBook-Air.local>
2025-05-21 12:40:36 -0400

    chore: cleanup

--- src/stories/components/cards/review-card/Index.stories.tsx
+++ src/stories/components/cards/review-card/Index.stories.tsx
@@ -1,11 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
 import { ReviewCard, ButtonProps } from './Index'; // Make sure this path is correct
-import './../../../styles/pages/global.scss';
+import '@/styles/pages/global.scss';
 import { JSX } from 'react';
 
 const meta = {
-  title: 'Components/Card/ReviewCard',
+  title: 'Components/Cards/ReviewCard',
   component: ReviewCard,
   parameters: {
     layout: 'centered',
