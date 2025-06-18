cb63a25876358b3527925c0ad4b807d01bbd7123
abdullahdoeswork <abdullahasadwork@gmail.com>
2025-06-18 13:16:36 -0400

    Vertical list component made

---
+++ src/stories/components/vertical-step-list/Index.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { fn } from '@storybook/test';
+import { Button, ButtonProps } from './Index'; // Make sure this path is correct
+import './../../../styles/pages/global.scss';
+import { JSX } from 'react';
+
+const meta = {
+  title: 'Components/VerticalStepList',
+  component: Button,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const Template = (args: JSX.IntrinsicAttributes & ButtonProps) => (
+  <div className="page">
+    <Button {...args} />
+  </div>
+);
+
+export const Primary: Story = {
+  render: Template, // Use the Template for rendering
+  args: {
+    primary: true,
+    label: 'Button',
+  },
+};
---
+++ src/stories/components/vertical-step-list/Index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import styles  from './style.module.scss';
+
+export interface ButtonProps {
+  onClick?: () => void;
+}
+
+
+
+export const Button = ({
+  
+}) => {
+  
+let x = [   {
+    headerText: 'Precision Integration',
+    subheaderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+    {
+    headerText: 'Seamless Collaboration',
+    subheaderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    headerText: 'Future Thinking',
+    subheaderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  },
+  {
+    headerText: 'Agile Approach',
+    subheaderText: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+  }
+]
+
+  return (
+    <>
+      <div className={styles['vertical-step-list-container']} >
+{x.map((step, index) => (
+    <div className={styles['timeline-container']} key={index}>
+      <div className={styles['circle']}>
+        {index + 1}
+      </div>
+      <div>
+        <div className={styles['vertical-step-list-header']}>
+          <b>
+            {step.headerText}
+          </b>
+          <div className={styles['vertical-step-list-header-subtitle']}>
+            <a>
+              {step.subheaderText}
+            </a>
+          </div>
+        </div>
+      </div>
+      </div>
+))}
+</div>
+</>
+)}
