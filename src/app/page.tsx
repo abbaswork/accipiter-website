84b119c729b2f0a75e1393a4a93f9b170a8c70c9
Abbas Khan <abbaskhanwork@gmail.com>
2025-06-23 00:14:46 +0400

    feat: add components to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,16 @@ import { ReactiveCard } from "@/stories/components/cards/interactive-card/Index"
 import { InteractiveCardStory } from "@/stories/components/cards/interactive-card/Index.stories";
 import { VerticalStepList } from "@/stories/components/vertical-step-list/Index";
 import { VerticalStepListStory } from "@/stories/components/vertical-step-list/Index.stories";
-
+import { CardRow } from "@/stories/components/cards/card/Index";
+import { Primary as CardRowArgs } from "@/stories/components/cards/card/card.stories";
 
 export default function Home() {
   return (
     <>
       <Hero {...HeroStory.args} />
-      <ReactiveCard  {...InteractiveCardStory.args} />
+      <ReactiveCard {...InteractiveCardStory.args} />
       <VerticalStepList {...VerticalStepListStory.args} />
+      <CardRow {...CardRowArgs.args} />
     </>
   );
 }
