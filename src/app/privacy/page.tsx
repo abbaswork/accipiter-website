8125708e985a93dc2a5496dcc0e11d3584e24627
Abbas Khan <abbaskhanwork@gmail.com>
2025-06-23 00:58:14 +0400

    feat: add coming soon pages

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,26 @@
+import { Hero } from "@/stories/components/hero/Index";
+
+const headerArgs = {
+  title: "Use Case Hero Title",
+  authorName: "John Doe",
+  authorPosition: "Software Engineer",
+  authorImage: "/author-icon.svg",
+};
+
+const HeroArgs = {
+  title: "Our Privacy Policy\n <span>Coming Soon</span>",
+  subtitle:
+    "We are just launching and will be adding more information about our services and expertise soon. Stay tuned!",
+  cta: {
+    label: "Book a Consultation",
+    href: "https://example.com/get-started",
+  },
+};
+
+export default function Contact() {
+  return (
+    <>
+      <Hero {...HeroArgs} />
+    </>
+  );
+}
