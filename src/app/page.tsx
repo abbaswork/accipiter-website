2be8d23e4aee41e0b88d6edc84c9502ee6e9139e
Abbas Khan <abbaskhanwork@gmail.com>
2025-06-22 19:42:23 +0400

    feat: hero logic on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,12 @@
 import styles from "./page.module.scss";
 import { Hero } from "@/stories/components/hero/Index";
+import { HeroStory } from "@/stories/components/hero/Index.stories";
 
-const HeroProps = {
-  title: "Legacy <span>Reimagined</span>,\nFuture-Proofed",
-  subtitle:
-    "Seamless headless migrations, AI-optimized ecosystems, and enterprise resilience. ",
-  logoSectionTitle: "Our Tech Stack",
-  logoBarImage: "/logo-bar.svg",
-  cta: {
-    label: "Book a Consultation",
-    href: "https://example.com/get-started",
-  },
-};
 
 export default function Home() {
   return (
     <>
-      <Hero {...HeroProps} />
+      <Hero {...HeroStory.args} />
     </>
   );
 }
