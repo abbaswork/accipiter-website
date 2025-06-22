33796445284ebd47db651628c14bb6237d67b5a0
Abbas Khan <abbaskhanwork@gmail.com>
2025-06-23 02:02:44 +0400

    feat: coming soon contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,19 @@
+import { UsecaseHero } from "@/stories/components/usecase-hero/Index";
+import { Hero } from "@/stories/components/hero/Index";
+import { HeroStory } from "@/stories/components/hero/Index.stories";
+import { HubSpotForm } from "@/stories/components/form/Index";
+
+const HeroArgs = {
+  title: "Contact Us to <span>Remodernize</span>,\n Your System for the Future",
+  subtitle:
+    "Please fill in the form below to get in touch with us. We look forward to hearing from you!",
+};
+
+export default function Contact() {
+  return (
+    <>
+      <Hero {...HeroArgs} />
+       <HubSpotForm/>
+    </>
+  );
+}
